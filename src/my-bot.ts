d238dad48f1441dcc857d15e4aede6d65ddd5ee7
David May-Miller <david.may-miller@softwire.com>
2017-07-20 15:05:19 +0100

    Made collision checking actually work

--- src/my-bot.ts
+++ src/my-bot.ts
@@ -21,12 +21,14 @@ export class MyBot {
         this.database = firebase.database();
         let exists: boolean = false;
         let counter = 0;
-        this.matchId = Math.floor(Math.random() * 1000) + 1;
         this.gameNum = 1;
         do {
+            this.matchId = Math.floor(Math.random() * 1000) + 1;
             this.database.ref('matches/' + this.matchId.toString()).once('value').then((snapshot) => {
                 if (snapshot.val()) {
                     exists = true;
+                } else {
+                    exists = false;
                 }
             });
             counter++;
