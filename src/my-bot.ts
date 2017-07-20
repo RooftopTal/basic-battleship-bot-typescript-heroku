c6b68d70594851ab18e7b4711ae1d2222cd734ab
David May-Miller <david.may-miller@softwire.com>
2017-07-20 14:00:47 +0100

    Separated getShipPositions into methods for each ship

--- src/my-bot.ts
+++ src/my-bot.ts
@@ -1,22 +1,22 @@
-import * as firebase from 'firebase';
+//import * as firebase from 'firebase';
 import { Position } from './interfaces/position';
 import { ShipPlace } from './interfaces/shipPlace';
 import { Shot } from './interfaces/shot';
 
 export class MyBot {
 
-    private database;
+    /*private database;
     private config = {
         apiKey: "AIzaSyD6ACQdu7gK-BgtJs-3Hu1Lkczk8fp0Abo",
         authDomain: "brokenbot-battleships.firebaseapp.com",
         databaseURL: "https://brokenbot-battleships.firebaseio.com",
         storageBucket: "brokenbot-battleships.appspot.com"
-    };
+    };*/
     private gameNum: number;
     private matchId: number;
 
     constructor() {
-        firebase.initializeApp(this.config);
+        /*firebase.initializeApp(this.config);
         this.authenticate();
         this.database = firebase.database();
         let exists: boolean = false;
@@ -34,17 +34,17 @@ export class MyBot {
         } while (exists)
         this.database.ref('matches/' + this.matchId.toString()).set({
             started: true
-        });
+        });*/
     }
     
     public getShipPositions() {
         
         return [
-            { StartingSquare: { Row: "A", Column: 1 }, EndingSquare : { Row: "A", Column: 5 } },
-            { StartingSquare: { Row: "C", Column: 1 }, EndingSquare : { Row: "C", Column: 4 } },
-            { StartingSquare: { Row: "E", Column: 1 }, EndingSquare : { Row: "E", Column: 3 } },
-            { StartingSquare: { Row: "G", Column: 1 }, EndingSquare : { Row: "G", Column: 3 } },
-            { StartingSquare: { Row: "I", Column: 1 }, EndingSquare : { Row: "I", Column: 2 } },
+            this.getAirCarrierPlace(),
+            this.getBattleshipPlace(),
+            this.getDestroyerPlace(),
+            this.getSubmarinePlace(),
+            this.getPatrolPlace(),
         ]
     }
 
@@ -74,11 +74,31 @@ export class MyBot {
         return column % 10 + 1;
     }
 
-    private authenticate(): void {
+    /*private authenticate(): void {
         firebase.auth().signInWithEmailAndPassword(
             "david.may-miller@softwire.com",
             "securePassword3"
         );
+    }*/
+
+    private getAirCarrierPlace(): ShipPlace {
+        return { StartingSquare: { Row: "A", Column: 1 }, EndingSquare : { Row: "A", Column: 5 } }
+    }
+
+    private getBattleshipPlace(): ShipPlace {
+        return { StartingSquare: { Row: "C", Column: 1 }, EndingSquare : { Row: "C", Column: 4 } }
+    }
+
+    private getDestroyerPlace(): ShipPlace {
+        return { StartingSquare: { Row: "E", Column: 1 }, EndingSquare : { Row: "E", Column: 3 } }
+    }
+
+    private getSubmarinePlace(): ShipPlace {
+        return { StartingSquare: { Row: "G", Column: 1 }, EndingSquare : { Row: "G", Column: 3 } }
+    }
+
+    private getPatrolPlace(): ShipPlace {
+        return { StartingSquare: { Row: "I", Column: 1 }, EndingSquare : { Row: "I", Column: 2 } }
     }
 }
 
