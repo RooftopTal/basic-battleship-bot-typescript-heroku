f9d406486f2c1247b026b43b0a3ea2cce611133d
James Hargreaves <James.Hargreaves@softwire.com>
2017-07-20 14:08:02 +0100

    Systematic destruction of ship v1

---
+++ src/Position.ts
@@ -0,0 +1,6 @@
+export class Position
+{
+    public constructor(public Row:string, public Column:number){
+
+    }
+}
--- src/my-bot.ts
+++ src/my-bot.ts
@@ -1,4 +1,14 @@
+import {Position} from './Position'
+
 export class MyBot {
+
+    private stateHitShipButNotSunk = false;
+    private stateKnowShipDirection = false;
+    private stateHorizontalShip = false;
+    private stateWalkingPositiveAxis = false;
+    private hitArray:Position[]; 
+    private missArray:Position[] = []; 
+
     public getShipPositions() {
         return [
             { StartingSquare: { Row: "A", Column: 1 }, EndingSquare : { Row: "A", Column: 5 } },
@@ -9,21 +19,238 @@ export class MyBot {
         ]
     }
 
-    public selectTarget(gamestate) {
-        var previousShot = gamestate.MyShots && gamestate.MyShots[gamestate.MyShots.length-1];
+    public selectTarget(gameState) {
+        var previousShot = gameState.MyShots && gameState.MyShots[gameState.MyShots.length-1];
         if(previousShot) {
-            return this.getNextTarget(previousShot.Position);
+
+            if(previousShot.WasHit())
+            {
+                if(this.hitArray)this.hitArray[this.hitArray.length] = previousShot.position;
+                else this.hitArray = [previousShot.position];
+                if(this.stateHitShipButNotSunk)
+                {
+                    //second hit on ship so can find direction
+                    let positionHitBefore;
+                    let onShot;
+                    for(let i =2; i <= 5; i++){
+                        if(gameState.MyShots[gameState.MyShots.length-i].WasHit){
+                            positionHitBefore =gameState.MyShots[gameState.MyShots.length-i];
+                            onShot = i;
+                            break; 
+                        }
+                    }
+                    this.stateHorizontalShip = this.shipHorizontal(previousShot.position,positionHitBefore);
+                    this.stateKnowShipDirection = true;
+                    this.stateWalkingPositiveAxis = (onShot == 2 || onShot == 3);
+                }
+
+                if(this.stateKnowShipDirection){
+                    //walk along ship in correct direction ********
+                    if(this.stateHorizontalShip){
+                        if(this.stateWalkingPositiveAxis){
+                            //next guess to right
+                            let nextShot:Position =  new Position(previousShot.position.Row, this.getRightColumn(previousShot.position.Column));
+                            if(this.alreadyHitAt(nextShot) || this.alreadyMissAt(nextShot) || nextShot.Column == 1){
+                                //clearly to the right is a miss or not on screen so need to find next left guess
+                                this.stateWalkingPositiveAxis = false;
+                                let noMissLeft = false;
+                                for(let i = 2; i <= 4; i ++){
+                                    //test position i positions left of current guess
+                                    nextShot = new Position(previousShot.position.Row, this.getLeftColumn(nextShot.Column));
+                                    if(this.alreadyMissAt(nextShot)){
+                                        // sunk************
+                                    }
+                                    else if(!this.alreadyHitAt){
+                                        //havent shot at this position before
+                                        break;
+                                    }
+                                    else if(i == 4){
+                                         noMissLeft = true;
+                                    }
+                                }
+                                if(noMissLeft){
+                                    //sunk ****************
+                                }
+                                else return nextShot;
+                            }
+                            else{
+                                //can just choose position
+                                return nextShot;
+                            }
+                        }
+                        else{
+                            //next guess to left
+                            let nextShot:Position =  new Position(previousShot.position.Row, this.getLeftColumn(previousShot.position.column));
+                            if(this.alreadyMissAt(nextShot)){
+                                //sunk ***********
+                            }
+                            return nextShot;
+                        }
+                    }
+                    else{
+                        //vertical ship
+                        if(this.stateWalkingPositiveAxis){
+                            //next guess up
+                            let nextShot:Position =  new Position(this.getUpRow(previousShot.position.Row), previousShot.position.Column);
+                            if(this.alreadyHitAt(nextShot) || this.alreadyMissAt(nextShot) || nextShot.Column == 1){
+                                //clearly to the right is a miss or not on screen so need to find next left guess
+                                this.stateWalkingPositiveAxis = false;
+                                let noMissDown = false;
+                                for(let i = 2; i <= 4; i ++){
+                                    //test position i positions left of current guess
+                                    nextShot =  new Position(this.getDownRow(nextShot.Row), previousShot.position.Column);
+                                    if(this.alreadyMissAt(nextShot)){
+                                        // sunk************
+                                    }
+                                    else if(!this.alreadyHitAt){
+                                        //havent shot at this position before
+                                        break;
+                                    }
+                                    else if(i == 4){
+                                         noMissDown = true;
+                                    }
+                                }
+                                if(noMissDown){
+                                    //sunk ****************
+                                }
+                                else return nextShot;
+                            }
+                            else{
+                                //can just choose position
+                                return nextShot;
+                            }
+                        }
+                        else{
+                            //next guess down
+                            let nextShot:Position =  new Position(this.getDownRow(previousShot.position.Row), previousShot.position.Column);
+                            if(this.alreadyMissAt(nextShot)){
+                                //sunk ***********
+                            }
+                            return nextShot;
+                        }
+                    }
+                }
+                else{
+                    //find the direction by hitting around the hit
+                    return this.hitButUnknownDirection(previousShot);
+                }
+            }
+            else
+            {
+                //not hit
+
+                if(this.missArray)this.missArray[this.missArray.length] = previousShot;
+                else this.missArray = [previousShot];
+
+                if(!this.stateHitShipButNotSunk)return this.getNextTarget(gameState, previousShot);
+                else if(!this.stateKnowShipDirection){
+                    let hitPosition;
+                    for(let i = 2; i <= 5; i ++){
+                        if(gameState.MyShots[gameState.MyShots.length-i].WasHit){
+                            hitPosition =gameState.MyShots[gameState.MyShots.length-i];
+                            break; 
+                        }
+                    }
+                    return this.hitButUnknownDirection(hitPosition);
+                }
+                else
+                {
+                    //end of ship come to *******check other end or sunk
+                }
+            }
+
+            return this.getNextTarget(gameState,previousShot.Position);
         }
         return { Row: "A", Column: 1 };  
     }
 
-    private getNextTarget(position) {
-        var column = this.getNextColumn(position.Column);
-        var row = column === 1 ? this.getNextRow(position.Row) : position.Row;
-        return { Row: row, Column: column }
+    public alreadyHitAt(pos:Position):boolean{
+        for(let i=0 ; i < this.hitArray.length; i ++){
+            if(this.hitArray[i] == pos) return true;
+        }
+        return false;
     }
 
-    private getNextRow(row) {
+    public alreadyMissAt(pos:Position):boolean{
+        for(let i=0 ; i < this.hitArray.length; i ++){
+            if(this.missArray[i] == pos) return true;
+        }
+        return false;
+    }
+
+
+    private hitButUnknownDirection(hitPosition:Position):Position{
+        var nextShot ={Row:this.getUpRow(hitPosition.Row), Column: hitPosition.Column};
+        if(this.alreadyMissAt(nextShot)){
+            nextShot = {Row:hitPosition.Row, Column: this.getRightColumn(hitPosition.Column)};
+            if(this.alreadyMissAt(nextShot)){
+                nextShot ={Row:this.getDownRow(hitPosition.Row), Column: hitPosition.Column};
+                if(this.alreadyMissAt(nextShot)){
+                    nextShot = {Row:hitPosition.Row, Column: this.getLeftColumn(hitPosition.Column)};
+                }
+            }
+        }
+        return nextShot;
+    }
+
+
+    private shipHorizontal(shot1:Position, shot2:Position){
+        if(shot1.Row == shot2.Row){
+            return true;
+        }
+        return false;
+    }
+
+
+    // private lastShotHit(gameState, previousShot){
+    //     //note this currently assumes that if two shots hit then they were adjacent so same ship *********
+    //     if(gameState.MyShots[gameState.length-2].WasHit){
+    //         //hit two in a row
+    //         var shotBefore = gameState.MyShots[gameState.length-2];
+    //         if(shotBefore.Position.Column == previousShot.Position.Column){
+    //             //shots in the same column so ship is verticle so try next;
+    //             if(shotBefore.Row.charCodeAt(0) >= previousShot.Row.charCodeAt(0)+1){
+    //                 //shot sequence is going down --assume top of boat is found
+    //                 var position = {Row : this.getDownRow(previousShot.Row), Column : shotBefore.position.Column}
+    //                 if(!gameState.MyShots.includes(position)) return position;
+    //                 else return this.getNextTarget(gameState,previousShot);
+    //             }
+    //             else{
+    //                 var position = {Row : this.getUpRow(previousShot.Row), Column : shotBefore.position.Column}
+    //                 if(!gameState.MyShots.includes(position)) return position;
+    //                 else {
+    //                     position = {Row : this.findBottomOfHits(gameState).Row, Column:shotBefore.position.Column};
+    //                     if(!gameState.MyShots.includes(position)) return position;
+    //                     else return this.getNextTarget(gameState,previousShot);
+    //                 }
+    //             }
+    //         }
+    //         else
+    //         {
+    //             //shots in horizontal sequence
+    //         }
+    //     }
+    // }
+
+    private findBottomOfHits(gameState):Position
+    {
+        var bottomPos;
+        for(let i = 1; i < gameState.length; i ++){
+            if(gameState.MyShots[gameState.length-i].WasHit) bottomPos = gameState.MyShots[gameState.length-i].position;
+        }
+        return bottomPos;
+    }
+
+    private getNextTarget(gameState,position):Position {
+        var column = this.getRightColumn(position.Column);
+        var row = column === 1 ? this.getDownRow(position.Row) : position.Row;
+        var nextPos = { Row: row, Column: column };
+        if(!gameState.MyShots.includes(position)) return position;
+        else this.getNextTarget(gameState,nextPos);
+
+    }
+
+    private getDownRow(row) {
         var newRow = row.charCodeAt(0) + 1;
         if(newRow > 'J'.charCodeAt(0)) {
             return 'A';
@@ -31,8 +258,20 @@ export class MyBot {
         return String.fromCharCode(newRow);
     }
 
-    private getNextColumn(column) {
+    private getUpRow(row) {
+        var newRow = row.charCodeAt(0) - 1;
+        if(newRow < 'A'.charCodeAt(0)) {
+            return 'J';
+        }
+        return String.fromCharCode(newRow);
+    }
+
+    private getRightColumn(column) {
         return column % 10 + 1;
+    }
+
+    private getLeftColumn(column) {
+        return column % 10 - 1;
     }
 }
 
