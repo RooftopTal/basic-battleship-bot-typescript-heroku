29f2de8f8ff4f25fa1add643f79aa5e30be4b1dc
James Hargreaves <James.Hargreaves@softwire.com>
2017-07-21 09:45:47 +0100

    missed a fu... bracket

--- src/my-bot.ts
+++ src/my-bot.ts
@@ -136,7 +136,7 @@ export class MyBot {
                                     console.log("SUNK");
                                     this.state.stateHitShipButNotSunk = false;
                                     this.state.stateKnowShipDirection =false;
-                                    return this.getNextTarget(gameState,new Position(previousShot.Position.Row, previousShot.Position.Column);
+                                    return this.getNextTarget(gameState,new Position(previousShot.Position.Row, previousShot.Position.Column));
                                 }
                                 else {
                                     return nextShot;
