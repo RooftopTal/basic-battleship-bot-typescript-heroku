ae7e2021b2f2d7cf405a4445381e9d634b930e5a
James Hargreaves <James.Hargreaves@softwire.com>
2017-07-24 16:37:16 +0100

    cbn

--- src/my-bot.ts
+++ src/my-bot.ts
@@ -56,9 +56,9 @@ export class MyBot {
         mat.surroundVerticalShips();
         console.log(mat.board);
         console.log(mat.ships);
-       // if(mat.ships[4] || mat.ships[5])return TargetingMethods.targetMethodPlaceLargestShip(mat);
-        //else return TargetingMethods.targetMethodTryRandomBlackSquare(mat);
-        return TargetingMethods.targetMethodOpponentsMostRecentMiss(mat, gameState);
+        if(gameState.MyShots.length< 10)return TargetingMethods.targetMethodOpponentsMostRecentMiss(mat, gameState);
+        else if(mat.ships[4] || mat.ships[5])return TargetingMethods.targetMethodPlaceLargestShip(mat);
+        else return TargetingMethods.targetMethodTryRandomBlackSquare(mat);
     }
 }
 
